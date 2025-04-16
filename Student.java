b1d9ca7305eadf8131d652d98cb674f080daf99e
ayeshabagalkot <ayeshabagalkot@gmail.com>
2025-04-16 11:11:04 +0530

    New

---
+++ Student.java
@@ -0,0 +1,18 @@
+package JenkinsDemo;
+
+public class Student {
+    public static void main(String[] args) {
+        int USN = 032;
+        String name = "Sarah";
+        String program = "BCA";
+        String address = "Hubli, Karnataka.";
+
+        System.out.println();
+        System.out.println("********Student Information********");
+        System.out.println();
+        System.out.println(USN);
+        System.out.println(name);
+        System.out.println(program);
+        System.out.println(address);
+    }
+}
